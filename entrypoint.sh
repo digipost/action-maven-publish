b75efe002318e24700eb44f3ae32a95dd797a05a
Eivind Bergstøl <eivind@bergstol.no>
2019-11-28 16:43:13 +0100

    vi kjører deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,8 +16,8 @@ echo ${GPG_PRIVATE_KEY} | base64 --decode | gpg --batch --import
 # Deploy to OSSRH, which will automatically release to Central Repository
 cd $GITHUB_WORKSPACE
 mvn --no-transfer-progress versions:set -DnewVersion=$RELEASE_VERSION
-mvn clean package \
+mvn clean deploy \
 	--batch-mode \
 	--no-transfer-progress \
-	--activate-profiles build-sources-and-javadoc,sign-artifacts \
+	--activate-profiles build-sources-and-javadoc,sign-artifacts,release \
 	--settings /settings.xml
